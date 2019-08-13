608e0fe0a5e46742040fcc5db4404f75ba24acb8
Viviane Nonato <nonato.viviane@gmail.com>
2019-08-13 05:08:15 -0300

    Dialogflow fulfillment webhook AWS Lambda
    Dialogflow fulfillment webhook with ExpressJs on AWS Lambda

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const app = require('./app');
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context); };
+
